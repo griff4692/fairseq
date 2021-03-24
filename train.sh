57dcc5a099976661c3d0cdce29ee7f9b2e748d5a
Griffin Adams <griff4692@gmail.com>
2021-03-24 11:34:44 -0400

    Removed default hparams.  Increse words per gpu.

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 BART_RESTORE='/home/griffin/weights/bart.large/model.pt'
-BART_OUT_DIR='/home/griffin/weights/bart.finetune/'
+BART_OUT_DIR='/home/griffin/weights/debug/'
 ENCODER_JSON='/home/griffin/fairseq/data/gpt2/encoder_special_toks.json'
 VOCAB_BPE='/home/griffin/fairseq/data/gpt2/vocab.bpe'
 IN_DIR=/home/griffin/nlp/projects/kabupra/mimic/clinbart/bin
@@ -18,48 +18,39 @@ python fairseq_cli/train.py /home/griffin/bin \
 --lr [$LR] \
 --warmup-updates $WARMUP \
 --total-num-update $TOTAL_STEPS \
---update-freq=[8] \
---max-tokens=1024 \
---max-tokens-valid 1024 \
+--update-freq=[4] \
+--max-tokens=2048 \
+--max-tokens-valid=2048 \
 --tokens-per-sample=512 \
---max-source-positions=1024 \
---max-target-positions=1024 \
 --save-interval 1 \
 --save-interval-updates=5000 \
---keep-interval-updates 10 \
+--keep-interval-updates 5 \
 --no-epoch-checkpoints \
 --adam-betas="(0.9, 0.98)" \
 --num-workers 8 \
---log-format json \
+--log-format tqdm \
 --arch bart_large \
 --train-subset train \
 --valid-subset valid \
---best-checkpoint-metric loss \
 --task denoising \
 --sample-break-mode=eos \
 --seed=1992 \
---log-format=json \
 --fp16 \
 --bpe gpt2 \
---pooler-activation-fn tanh \
---share-all-embeddings \
---layernorm-embedding \
---share-decoder-input-output-embed \
 --optimizer adam \
---criterion cross_entropy \
---encoder-learned-pos \
---decoder-learned-pos \
 --skip-invalid-size-inputs-valid-test \
 --lr-scheduler polynomial_decay \
 --weight-decay=0.01 \
---power=1.0 \
 --mask=0.3 \
 --mask-random=0.1 \
---poisson-lambda=3.0 \
 --permute-sentences=1.0 \
 --mask-length=span-poisson \
 --replace-length=1 \
 --insert=0.0 \
 --rotate=0.0 \
---activation-fn=gelu \
---adam-eps=1e-06 \
+--adam-eps=1e-06;
+#--clip-norm=0.1 \
+#--attention-dropout 0.1 \
+#--criterion label_smoothed_cross_entropy \
+#--label-smoothing=0.1 \
+#--find-unused-parameters;
